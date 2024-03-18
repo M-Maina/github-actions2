a65888bea3349f150b8a6bfd75453664643b76da
Geoffrey M Maina <pythondeveloper21@gmail.com>
2024-03-18 13:06:16 -0700

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,10 @@
+$ErrorActionPreference = 'stop'
+
 $StackName = 'awsvpcstack'
 $TemplateBody = Get-Content -Path cft-vpc.yml -Raw
 
+if ((Get-CFNStack -StackName)) {
+Update-CFNStack -StackName $StackName -TemplateBody $TemplateBody
+} else {
 New-CFNStack -StackName $StackName -TemplateBody $TemplateBody
+}
